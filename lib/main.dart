0cb80370318dabed9ecbc7b98ce65fc984def48c
pguilimaf <pguilimaf@gmail.com>
2021-06-25 13:32:49 -0300

    Salvando as Tarefas Permanentemente

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,17 @@ class _HomeState extends State<Home> {
 
   List _toDoList = [];
 
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
+
   void _addToDo() {
     setState(() {
       Map<String, dynamic> newToDo = Map();
@@ -27,6 +38,7 @@ class _HomeState extends State<Home> {
       _toDoController.text = "";
       newToDo["ok"] = false;
       _toDoList.add(newToDo);
+      _saveData();
     });
   }
 
@@ -77,6 +89,7 @@ class _HomeState extends State<Home> {
                   onChanged: (check) {
                     setState(() {
                       _toDoList[index]["ok"] = check;
+                      _saveData();
                     });
                   },
                 );
